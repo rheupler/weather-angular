b962c6bc7e88a4d391bfbf444282e7c45f9b2ddf
ryan heupler <rheupler@gmail.com>
2016-08-22 13:42:11 -0700

    adds working weather api integration for city temps

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ weatherApp.config(function ($routeProvider) {``
     templateUrl: 'pages/forecast.htm',
     controller: 'forecastController'
   })
+
+  .when('/forecast/:days', {
+    templateUrl: 'pages/forecast.htm',
+    controller: 'forecastController'
+  })
 });
 
 // SERVICES
@@ -32,14 +37,24 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
   });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 
   $scope.city = cityService.city;
 
+  $scope.days = $routeParams.days || 2;
+
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=6c23fe5cadc19522c91a039f295ca89e", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
-  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
 
-  console.log($scope.weatherResult);
+  $scope.convertToFarenheit = function(degK) {
+
+    return Math.round((1.8 * (degK - 273)) + 32);
+
+  }
+
+  $scope.convertToDate = function(dt) {
+    return new Date(dt * 1000);
+  }
 
 }]);
