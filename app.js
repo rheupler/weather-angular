05cb61e989efbf4cea41ac546bd0bfaa985ed825
ryan heupler <rheupler@gmail.com>
2016-08-22 14:19:30 -0700

    adds custom directive for weather

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
 
   $scope.city = cityService.city;
 
-  $scope.days = $routeParams.days || 2;
+  $scope.days = $routeParams.days || '2';
 
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=6c23fe5cadc19522c91a039f295ca89e", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
@@ -58,3 +58,18 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
   }
 
 }]);
+
+// DIRECTIVES
+weatherApp.directive('weatherReport', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/weatherReport.html',
+    replace: true,
+    scope: {
+      weatherDay: "=",
+      convertToStandard: "&",
+      convertToDate: "&",
+      dateFormat: "@"
+    }
+  }
+})
