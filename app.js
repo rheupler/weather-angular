ad575a5e543dd394d52773c33c67ecfc16a7a563
ryan heupler <rheupler@gmail.com>
2016-08-22 13:20:22 -0700

    adds initial files and api for weather data

---
+++ app.js
@@ -0,0 +1,45 @@
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+weatherApp.config(function ($routeProvider) {``
+
+  $routeProvider
+
+  .when('/', {
+    templateUrl: 'pages/home.htm',
+    controller: 'homeController'
+  })
+
+  .when('/forecast', {
+    templateUrl: 'pages/forecast.htm',
+    controller: 'forecastController'
+  })
+});
+
+// SERVICES
+weatherApp.service('cityService', function() {
+  this.city = "New York, NY";
+});
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+
+  $scope.city = cityService.city;
+
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  });
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+
+  $scope.city = cityService.city;
+
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=6c23fe5cadc19522c91a039f295ca89e", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+
+  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+
+  console.log($scope.weatherResult);
+
+}]);
