e5282d61acc0ec2fdd72d65b6f43115f44e30ab0
ryan heupler <rheupler@gmail.com>
2016-08-24 14:19:39 -0700

    adds ui tweaks

--- controllers.js
+++ controllers.js
@@ -1,22 +1,24 @@
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', 'cityService', '$location', function($scope, cityService, $location) {
 
   $scope.city = cityService.city;
 
   $scope.$watch('city', function() {
     cityService.city = $scope.city;
   });
+
+  $scope.submit = function() {
+    $location.path('/forecast')
+  }
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', function($scope, $routeParams, cityService, weatherService) {
 
   $scope.city = cityService.city;
 
   $scope.days = $routeParams.days || '2';
 
-  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=6c23fe5cadc19522c91a039f295ca89e", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
-
-  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+  $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
 
   $scope.convertToFarenheit = function(degK) {
 
