50a0c236134eec7653fc62b22115447ddb451cca
ryan heupler <rheupler@gmail.com>
2016-08-24 15:57:54 -0700

    fix attempt for api not hitting

--- services.js
+++ services.js
@@ -5,7 +5,7 @@ weatherApp.service('cityService', function() {
 
 weatherApp.service('weatherService', ['$resource', function($resource) {
   this.GetWeather = function(city, days) {
-  var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=6c23fe5cadc19522c91a039f295ca89e", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+  var weatherAPI = $resource("//api.openweathermap.org/data/2.5/forecast/daily?APPID=6c23fe5cadc19522c91a039f295ca89e", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
   return weatherAPI.get({ q: city, cnt: days });
   }
